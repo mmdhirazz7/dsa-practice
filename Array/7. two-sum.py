086ddc6b156fe3ef8823247d63d11de002050fc2
mmdhirazz7 <mmdhirazz7@gmail.com>
2025-07-03 18:53:33 +0530

    Added description

--- Array/7. two-sum.py
+++ Array/7. two-sum.py
@@ -1,13 +1,20 @@
 def twoSum(nums, target):
-    """ Takes the list of integers and a target number, 
-    and returns the indices of the list in the order they are stored in the list 
-    whose values add up to target"""
+    """ Finds two distinct indices in the input list such that the sum of the corresponding values equals the target.
+    
+    parameters : 
+                nums (List[int]) : a list of integers
+                target (int) : the target sum to find.
+    returns :
+        List[int] : A List containing two indices [i,j] where nums[i] + nums[j] == target, and i < j"""
+
+    # Brute force method
     # for i in range(len(nums)):
         #     for j in range(i+1,len(nums)):
         #         if nums[i] + nums[j] == target:
         #             result[0] = i
         #             result[1] = j
         # return result
+        
     seen = {}
     for i, num in enumerate(nums):
         if target - num in seen :
